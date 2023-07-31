b7ddea064290cf8a5d39901c4b1e5119215f691f
jedllorente <jedllorente@gmail.com>
2023-07-31 16:00:05 +0800

    working with math operations and javascript math rules

--- operations.js
+++ operations.js
@@ -1,6 +1,23 @@
+// Math operations / operators
+
 console.log(10 + 4); //addition
 console.log(10 - 4); //subtraction
 console.log(10 * 4); //multiplication
 console.log(10 / 4); //division
 
-console.log(10 % 4); //modulus / modulo operator
+console.log(10 % 4); //modulus / modulo operator : - this will always give us the remainder of the divided units. e.g.: 10 / 4 => 2 why? 4 + 4 = 8
+                     //and hence to get the total number of "10" we have to add up 2 more. 8 + 2 = 10.
+
+
+// Math Rules.
+
+// Incrementation
+let result = (10 + 3 - 5) * 10;
+result++; //result = result + 1;
+result--; //result = result - 1;
+
+
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result *= 5; // result = result * 5
+result /= 5; // result = result / 5
