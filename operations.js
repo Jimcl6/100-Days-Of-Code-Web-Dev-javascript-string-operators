3ef028ecfb8082425f333e2ef9e8c71263fe6521
jedllorente <jedllorente@gmail.com>
2023-07-31 14:46:26 +0800

    modulo operator

--- operations.js
+++ operations.js
@@ -2,3 +2,5 @@ console.log(10 + 4); //addition
 console.log(10 - 4); //subtraction
 console.log(10 * 4); //multiplication
 console.log(10 / 4); //division
+
+console.log(10 % 4); //modulus / modulo operator
