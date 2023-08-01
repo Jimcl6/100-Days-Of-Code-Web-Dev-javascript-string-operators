d1682d126370b6e04c69c5832bcbc2af60ca22c4
Jimcl6 <jedllorente@gmail.com>
2023-08-01 22:43:39 +0800

    working with basic array operators

--- operations.js
+++ operations.js
@@ -6,7 +6,7 @@ console.log(10 * 4); //multiplication
 console.log(10 / 4); //division
 
 console.log(10 % 4); //modulus / modulo operator : - this will always give us the remainder of the divided units. e.g.: 10 / 4 => 2 why? 4 + 4 = 8
-                     //and hence to get the total number of "10" we have to add up 2 more. 8 + 2 = 10.
+//and hence to get the total number of "10" we have to add up 2 more. 8 + 2 = 10.
 
 
 // Math Rules.
@@ -35,5 +35,10 @@ console.log(10 % 4); //modulus / modulo operator : - this will always give us th
 
 
 // String operators
-console.log('Jed' + 'Llorente');
-console.log('Jed' - 'Llorente');
+console.log('Jed' + 'Llorente'); // this concatenantes(combines the strings)
+console.log('Jed' - 'Llorente'); // this will return NaN(Not-a-Number) because normally operators work only on numbers / integers / floats, the exception to this rule only works on string concatenation.
+
+let userName = 'Jed';
+
+console.log(userName.length); //the .length property allows us to count the number of elements inside a string or array.
+console.log(userName.toUpperCase()); //the .toUpperCase() is a method that allows us to convert the entirety of the string all to Upper Case letters.
